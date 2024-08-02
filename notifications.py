1be000c1d5bfee2353688cd44fee4e07efe3cfa0
gus <gus@pop-os.domain>
2024-08-01 18:58:03 -0600

    working notification system

--- notifications.py
+++ notifications.py
@@ -1,6 +1,12 @@
 import os
 import sqlite3
 from dotenv import load_dotenv
+import math
+from discord_webhook import DiscordWebhook
+
+earth_radius = 3960.0
+degrees_to_radians = math.pi/180.0
+radians_to_degrees = 180.0/math.pi
 
 load_dotenv()
 
@@ -15,14 +21,29 @@ def checkNotifications():
         WHERE alerts.status = true''')
     alerts = cursor.fetchall()
     for alert in alerts:
-        print(alert)
         cursor.execute('''
             SELECT * FROM device_history
             WHERE uuid = ?
             LIMIT 1''', (str(alert[1]),))
         location = cursor.fetchone()
-        print(location)
+        long_tol = change_in_longitude(location[3], alert[2])
+        lat_tol = change_in_latitude(alert[3])
+        if (abs(alert[3]) + abs(long_tol) <= location[3] or abs(alert[4]) + abs(lat_tol) <= location[4]):
+            # this one is fine
+            webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content='hello')
+            response = webhook.execute()
     cursor.close()
     
-    print(DISCORD_WEBHOOK_URL)
-    
+
+
+
+
+def change_in_latitude(miles):
+    "Given a distance north, return the change in latitude."
+    return (miles/earth_radius)*radians_to_degrees
+
+def change_in_longitude(latitude, miles):
+    "Given a latitude and a distance west, return the change in longitude."
+    # Find the radius of a circle around the earth at given latitude.
+    r = earth_radius*math.cos(latitude*degrees_to_radians)
+    return (miles/r)*radians_to_degrees
